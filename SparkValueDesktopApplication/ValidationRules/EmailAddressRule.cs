eddd141f47f92a994131da52b44d7de503f6e5e4
Joseph Baker <jbaker@student.neumont.edu>
2022-11-02 16:22:17 -0600

    Email address validation rule complete.

--- SparkValueDesktopApplication/ValidationRules/EmailAddressRule.cs
+++ SparkValueDesktopApplication/ValidationRules/EmailAddressRule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -12,6 +13,12 @@ namespace SparkValueDesktopApplication.ValidationRules
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
+            // Is the email valid?
+            if (!MailAddress.TryCreate((string)value, out var mailAddress))
+            {
+                return new ValidationResult(false, "Email address must be valid");
+            }
+
             return new ValidationResult(true, null);
         }
     }
