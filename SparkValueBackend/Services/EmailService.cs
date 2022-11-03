e78d3254c36180274d61eba7c87435218bfb4323
Joseph Baker <jbaker@student.neumont.edu>
2022-11-03 15:30:29 -0600

    Welcome email is stubbed out until I can test it.

---
+++ SparkValueBackend/Services/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using SparkValueBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkValueBackend.Services
+{
+    public class EmailService
+    {
+        private readonly string APIKey;
+        private readonly string SendingEmail;
+
+        public EmailService()
+        {
+            // API key is created here, in the future store in some other place secure
+            APIKey = "";
+            // Get our from address from some other secure palce
+            SendingEmail = "learningelectronics@sparkvalue.com";
+        }
+
+        public async Task SendWelcomeEmail(UserAccountModel user)
+        {
+            SendGridClient client = new SendGridClient(APIKey);
+            SendGridMessage msg = new SendGridMessage()
+            {
+                From = new EmailAddress(SendingEmail, "Spark Value Bot"),
+                Subject = "Welcome to Spark Value",
+                PlainTextContent = $"Welcome, {user.Username}, to the world of electronics! " +
+                                    "We hope that you learn a lot and have fun while doing so."
+            };
+            msg.AddTo(user.EmailAddress, user.Username);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+
+        public async Task SendPasswordResetEmail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
