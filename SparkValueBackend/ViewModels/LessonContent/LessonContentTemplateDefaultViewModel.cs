37ec596bc91fb372dbe5e235efc617ae28420ad2
Joseph Baker <jbaker@student.neumont.edu>
2022-11-09 12:53:39 -0700

    Lesson templates are ready for basic testing and implementation. Added default template if no interactive element is present.

---
+++ SparkValueBackend/ViewModels/LessonContent/LessonContentTemplateDefaultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkValueBackend.ViewModels.LessonContent
+{
+    public class LessonContentTemplateDefaultViewModel : LessonContentViewModelBase
+    {
+        public LessonContentTemplateDefaultViewModel(string plainText, string? interactiveElement) : base(plainText, interactiveElement)
+        {
+        }
+    }
+}
